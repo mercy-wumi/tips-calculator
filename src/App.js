eb0d29e1db297edcc897ad7ebbc2e4754e0f47e6
mercy-wumi <m.adams1909@gmail.com>
2021-10-12 14:09:23 +0100

    first commit on tips calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-green-200 text-white w-screen h-screen ">
+      <div className="pt-16">
+        <div className="text-xl uppercase text-center text-green-900">
+          <p className="tracking-widest">spli</p>
+          <p className="tracking-widest">tter</p>
+        </div>
+        <div className="mx-auto bg-white rounded-2xl p-4 w-3/5 mt-16">
+
+        </div>
+      </div>
+
     </div>
   );
 }
